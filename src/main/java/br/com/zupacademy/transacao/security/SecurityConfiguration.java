f56615d34540d709ed07b78b084df3dd82263b92
Sérgio Guimarães de Andrade <sergio.guimaraes.ads@gmail.com>
2021-06-23 18:43:14 -0300

    Autenticação e Autorização

---
+++ src/main/java/br/com/zupacademy/transacao/security/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package br.com.zupacademy.transacao.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@EnableWebSecurity
+@Configuration
+@Profile("prod")
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers(HttpMethod.GET, "/transacao/**").hasAuthority("SCOPE_read")
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
