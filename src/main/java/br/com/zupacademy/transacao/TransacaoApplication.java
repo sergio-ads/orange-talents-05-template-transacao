3dc6dac8f7efa6bbcafa4e078b6d888f48c9527c
Sérgio Guimarães de Andrade <sergio.guimaraes.ads@gmail.com>
2021-06-23 14:15:11 -0300

    Corrigir TransacaoApplication

--- src/main/java/br/com/zupacademy/transacao/TransacaoApplication.java
+++ src/main/java/br/com/zupacademy/transacao/TransacaoApplication.java
@@ -1,8 +1,5 @@
 package br.com.zupacademy.transacao;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TransacaoApplication {
 
 	public static void main(String[] args) {
-		System.out.println(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.SSS").format(LocalDateTime.now()));
 		SpringApplication.run(TransacaoApplication.class, args);
 	}
 
